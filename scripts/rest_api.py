705ec919268d38ab6f2339800631b02c8104e926
Derek Chen <derekchen14@gmail.com>
2019-02-18 09:54:57 -0800

    Finished the REST API, can now run end to end

--- scripts/rest_api.py
+++ scripts/rest_api.py
@@ -1,30 +1,54 @@
 import requests
 import json
+import pdb
 
 def make_request(action):
-  url = "http://18.218.116.164:8000/admin/djangoApp/studentinfo/"
+  url = "http://18.218.116.164:8000/djangoApp/"
 
   if action == "create":
-    payload = {"student_id": 10, "student_year": 2017,
-               "student_name": "John Smith",
-               "registered_courses": "distributed computing"}
-    result = requests.post(url, params)
-  elif action == "read ":
+    payload = {"STUDENT_ID": 8, "STUDENT_YEAR": 2019,
+               "STUDENT_NAME": "Ishan Ishikawa",
+               "REGISTERED_COURSES": "Intelligence of an Artificial Nature"}
+    result = requests.post(url, data=payload)
+  elif action == "read":
+    student_id = 3
+    url = url + str(student_id)
     result = requests.get(url)
   elif action == "update":
-    payload = {"student_id": 10,
-               "student_name": "Barack Obama"}
-    result = requests.put(url, params)
-  elif action == "delete":
-    params = {"student_id": 10}
-    result = requests.delete(url, params)
+    student_id = 8
+    url = url + str(student_id)
+    payload = {
+    "STUDENT_ID": 8,
+    "STUDENT_NAME": "Ishan Ishikawa",
+    "STUDENT_YEAR": 2019,
+    "REGISTERED_COURSES": "Artificial Intelligence"}
+    result = requests.put(url, data=payload)
+  elif action == "destroy":
+    student_id = 8
+    url = url + str(student_id)
+    result = requests.delete(url)
+  elif action == "list":
+    result = requests.get(url)
 
-  return result.json() if result.status_code == 200 else result.error_message
+  if result.status_code in [200, 201, 202, 203, 204]:
+    return result
+  else:
+    result.raise_for_status()
 
+action_mapping = {
+    "create": "creating",
+    "read": "reading",
+    "update": "updating",
+    "destroy": "deleting",
+}
 
 if __name__ == "__main__":
-  for action in ["read ", "create", "update", "delete"]:
+  for action in ["read", "create", "update", "destroy"]:
     result = make_request(action)
-    gerund_form = action[:-1] + "ing"
-    print("result from {}:".format(gerund_form))
-    print(json.dumps(result, indent=4))
+    print("Raw result from {}: ".format(action_mapping[action]) )
+    print(result.text)
+
+    action = "read"
+    listing = make_request("list")
+    print("Full table after {}: ".format(action_mapping[action]) )
+    print(json.dumps(listing.json(), indent=4))
